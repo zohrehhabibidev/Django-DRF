f7876a50b4e5f7c47a862eb3346e5b31ce872e98
Zohreh Habibi <habibi.z@outlook.de>
2026-09-21 15:12:22 +0200

    feat: add market seller and product model

--- market_app/models.py
+++ market_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Market(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+    net_worth = models.DecimalField(max_digits=100, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+
+class Seller(models.Model):
+    name = models.CharField(max_length=255)
+    contact_info = models.TextField()
+    markets = models.ManyToManyField(Market, related_name='sellers')
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=50, decimal_places=2)
+    market = models.ForeignKey(
+        Market, on_delete=models.CASCADE, related_name='products')
+    seller = models.ForeignKey(
+        Seller, on_delete=models.CASCADE, related_name='products')
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
--- supermarket/settings.py
+++ supermarket/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'market_',
+    'market_app',
 ]
 
 MIDDLEWARE = [
