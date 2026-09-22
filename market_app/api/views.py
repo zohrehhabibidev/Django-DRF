235893554c8732e373d21891d5bdcf22b64b6293
Zohreh Habibi <habibi.z@outlook.de>
2026-09-22 10:11:36 +0200

    feat: add HTTP status codes to API responses

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 @api_view(['GET', 'POST'])
@@ -11,6 +12,6 @@ def first_view(request):
     if request.method == 'POST':
         try:
             msg = request.data['message']
-            return Response({'your_message': msg})
+            return Response({'your_message': msg}, status=status.HTTP_201_CREATED)
         except:
-            return Response({'message': 'error'})
+            return Response({'message': 'error'}, status=status.HTTP_400_BAD_REQUEST)
