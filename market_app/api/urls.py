cb7e50143f52ebc104080275aa655f6eab149ac1
Zohreh Habibi <habibi.z@outlook.de>
2026-09-23 09:38:18 +0200

    feat: add single market view and delete

--- market_app/api/urls.py
+++ market_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import market_view
+from .views import market_view, market_single_view
 
 urlpatterns = [
     path('', market_view),
+    path('<int:pk>/', market_single_view),
 ]
--- market_app/api/views.py
+++ market_app/api/views.py
@@ -19,3 +19,23 @@ def market_view(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'DELETE',])
+def market_single_view(request, pk):
+
+    if request.method == 'GET':
+        try:
+            market = Market.objects.get(pk=pk)
+            serializer = MarketSerializer(market)
+            return Response(serializer.data)
+        except Market.DoesNotExist:
+            return Response({'message': 'Market not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'DELETE':
+        try:
+            market = Market.objects.get(pk=pk)
+            market.delete()
+            return Response({'message': 'Market deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except Market.DoesNotExist:
+            return Response({'message': 'Market not found'}, status=status.HTTP_404_NOT_FOUND)
